b56bee9e943688af16a9ba5e9304a3e49f9a72a5
edgar_briceno <edgar.briceno@oracle.com>
2020-08-26 17:21:24 -0500

    comments about code

--- Aug-LeetCode-Challenge/week4/D3.StreamofCharacters.java
+++ Aug-LeetCode-Challenge/week4/D3.StreamofCharacters.java
@@ -31,7 +31,28 @@ Queries will only consist of lowercase English letters.
 The number of queries is at most 40000.
 https://leetcode.com/explore/featured/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3434/
 */
+/*
+the stream of characters keeps adding the new characters to the queue and it returns true for any part of the string that 
+is present in the Trie.
+example;
+words. ["cd","f","kl"]
+streamChecker.query('a');          // return false                                      a
+streamChecker.query('b');          // return false                                      ab
+streamChecker.query('c');          // return false                                      abc
+streamChecker.query('d');          // return true, because 'cd' is in the wordlist      abcd
+streamChecker.query('e');          // return false                                      abcde
+streamChecker.query('f');          // return true, because 'f' is in the wordlist       abcdef
+streamChecker.query('g');          // return false                                      abcdefg
+streamChecker.query('h');          // return false                                      abcdefgh
+streamChecker.query('i');          // return false                                      abcdefghi
+streamChecker.query('j');          // return false                                      abcdefghij
+streamChecker.query('k');          // return false                                      abcdefghijk
+streamChecker.query('l');          // return true, because 'kl' is in the wordlist      abcdefghijkl
 
+the strategy is to save the words in a data structure that keeps an easy access to the words we already know.
+ and to keed a string or another data structure with the current stream of characters to compare agains the trie.
+  once we reach an end of word in the trie then we are good to go.
+*/
 class StreamChecker {
     
     class Trie{
@@ -65,9 +86,12 @@ class StreamChecker {
     }
     
     public Trie t = new Trie();
+    // this deque helps use keep appending new characters to the queue
     public Deque<Character> stream = new LinkedList();
     
     public StreamChecker(String[] words) {
+        // add the words in reverse order for them to be faster to retrieve and find in the trie.
+        // because the way they are stored now you can do searches faster.
         for(String w: words){
             String rev = new StringBuilder(w).reverse().toString();
             t.insert(rev);
