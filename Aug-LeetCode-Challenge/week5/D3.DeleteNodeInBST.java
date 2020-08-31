b55cacf351ac000120d87f13835a74594502f953
edgar_briceno <edgar.briceno@oracle.com>
2020-08-31 12:11:59 -0500

    day 3 delete from bst

---
+++ Aug-LeetCode-Challenge/week5/D3.DeleteNodeInBST.java
@@ -0,0 +1,89 @@
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+Note: Time complexity should be O(height of tree).
+
+Example:
+
+root = [5,3,6,2,4,null,7]
+key = 3
+
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Given key to delete is 3. So we find the node with value 3 and delete it.
+
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+
+    5
+   / \
+  4   6
+ /     \
+2       7
+
+Another valid answer is [5,2,6,null,4,null,7].
+
+    5
+   / \
+  2   6
+   \   \
+    4   7
+https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/553/week-5-august-29th-august-31st/3443/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return null;
+        }
+        else if(key < root.val){
+            root.left = deleteNode(root.left, key);
+        }
+        else if(key > root.val){
+            root.right = deleteNode(root.right, key);
+        }
+        else{
+            System.out.println("found: " + key);
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+            root.val = inOrderSuccessor(root.right);
+            root.right = deleteNode(root.right, root.val);
+            
+        }
+        return root;
+    }
+    
+    public int inOrderSuccessor(TreeNode root){
+        int min = root.val;
+        while(root.left != null){
+            root = root.left;
+            min = root.val;
+        }
+        return min;
+    }
+}
