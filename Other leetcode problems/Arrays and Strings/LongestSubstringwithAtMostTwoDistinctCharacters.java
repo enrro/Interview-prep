caec4fa99514a74e7dec9a84b3811599228bf7e8
enrro <edgarbg585@gmail.com>
2020-09-23 21:01:11 -0500

    updates to sliding window problem

--- Other leetcode problems/Arrays and Strings/LongestSubstringwithAtMostTwoDistinctCharacters.java
+++ Other leetcode problems/Arrays and Strings/LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -10,23 +10,28 @@ Example 2:
 Input: "ccaabbb"
 Output: 5
 Explanation: t is "aabbb" which its length is 5.*/
-
+// * * this is probably the best sliding window example there is
+// use a map to track number of apperences and amount of apperances
 class Solution {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         HashMap<Character, Integer> map = new HashMap();
-        int max = 0;
-        int slow = 0;
+        int max = 0, slow = 0;
         
         for(int i = 0; i < s.length(); i++){
+            // add the vale to the map
             char c = s.charAt(i);
             map.put(s.charAt(i), 1+map.getOrDefault(s.charAt(i),0));
             
+            // make sure the map the right size
             while(map.size() > 2){
-                map.put(s.charAt(slow), map.getOrDefault(s.charAt(slow),0)-1);
-                if(map.get(s.charAt(slow)) == 0) map.remove(s.charAt(slow));
+                char t = s.charAt(slow);
+                map.put(t, map.get(t)-1);
+                if(map.get(t) == 0) map.remove(t);
                 slow++;
             }
-            max = Math.max(max, i-slow+1);
+            
+            // get the max
+            max = Math.max(max, i - slow + 1);
         }
         
         return max;
